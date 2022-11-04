072e538beb75ed65fea6854ef0f3e36827b2baa1
rusefillc <sdfsdfqsf2334234234>
2022-11-04 15:09:23 -0400

    hello1

---
+++ src/main/java/ui/offtopic/Hello1.java
@@ -0,0 +1,11 @@
+package ui.offtopic;
+
+import javax.swing.*;
+
+public class Hello1 {
+    public static void main(String[] args) {
+        FrameHelper frameHelper = new FrameHelper();
+        JFrame frame = frameHelper.getFrame();
+        frame.setVisible(true);
+    }
+}
